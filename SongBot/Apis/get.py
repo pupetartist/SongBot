f7ad6fbd7fa061ce931a50492482fc1f3b77dd10
Diego Olvera <diego.olvera97@gmail.com>
2016-12-12 19:37:32 -0600

    agregando requests

---
+++ SongBot/Apis/get.py
@@ -0,0 +1,27 @@
+#-*-encode:utf-8 -*-
+import requests
+import json
+
+
+class Musix():
+
+	def __init__(self):
+		self.url = "http://api.musixmatch.com/ws/1.1/"
+		self.token = "a08b8cc21b558ca04935043a1edceca4"
+
+	def getTrack(self, lyrics):
+		self.url = "http://api.musixmatch.com/ws/1.1/track.search"
+		parametros = {"apikey": self.token, "q_lyrics": lyrics}
+		resultado = requests.get(self.url, params = parametros).json()
+		return resultado
+
+class Spotify():
+
+	def __init__(self):
+		self.url = "https://api.spotify.com/v1/"
+
+	def getDemo(self, song):
+		self.url = "https://api.spotify.com/v1/search"
+		parametros = {"q": song, "type": "track", "limit": "10"}
+		resultado = requests.get(self.url, params = parametros).json()
+		return resultado
